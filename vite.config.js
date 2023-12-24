82872d27f111498632ea5b8b775250839c17f4f1
Gabriel R. <gabrielrengifo211@gmail.com>
2023-12-23 21:47:03 -0400

    Added Tribute page, seems to be working

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,12 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'nested/prism-docs/index.html'),
+        'prism': './nested/prism-docs/index.html',
+        'tribute': './nested/tribute/index.html'
       },
     },
   },
